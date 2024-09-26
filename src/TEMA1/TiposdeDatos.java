f78f648e418b0ccd9490c44ba0babbfaa8650330
DiegoReyes06 <dreytov969@g.educaand.es>
2024-09-26 20:27:48 +0200

    Add files via upload

---
+++ src/TEMA1/TiposdeDatos.java
@@ -0,0 +1,16 @@
+package TEMA1;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class TiposdeDatos {
+    public static void main(String[] args) {
+        String nombre = "Javi";
+        char caracter = 'a';
+        System.out.println(caracter);
+
+        caracter = 74;
+        System.out.println(caracter); //Pinta la represntacion ASCII del numero 74->J
+
+    }
+
+}
